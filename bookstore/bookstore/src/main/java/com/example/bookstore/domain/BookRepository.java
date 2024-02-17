01a6307634c5e78d2ac68672fc977cf02e55d6d3
ilarikoik <ilari.koik@gmail.com>
2024-02-17 22:38:02 +0200

    eka

--- bookstore/bookstore/src/main/java/com/example/bookstore/domain/BookRepository.java
+++ bookstore/bookstore/src/main/java/com/example/bookstore/domain/BookRepository.java
@@ -4,8 +4,8 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {// book,long
-
+    // crud eli create read update delete
     List<Book> findByAuthor(String author);
+    // crudilla on metodeja valmiiks kuten findBy
 
 }
-/////
